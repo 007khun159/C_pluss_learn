1e401a85fd9a1f4d053532ba873a5a7ba94f0341
007khun159 <87358687+007khun159@users.noreply.github.com>
2022-11-30 14:49:01 +0700

    Add files via upload

---
+++ Datatype.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int number1 ,number2; 
+
+
+
+
+	number1 = 100;
+	number2 = 200;
+	//constant ไม่สามารถเปลี่ยนแปลงได้ในภายหลัง
+	const float scoree = 70.5;
+	string name = "Papop";
+
+
+
+	int age = 25, tall = 175;
+	bool pass = true, fail = false;
+	
+	
+	
+	
+
+	// output
+	cout << "Number1 = " << number1 << endl;
+	cout << "Number2 = " <<  number2 << endl;
+	cout << "Score = " <<  scoree << endl;
+	cout << "Name = " << name << endl;
+	cout << "Age is " << age << " He tall " << tall <<endl;
+	cout << "The exam was " << pass << " but Student was " << fail;
+
+}
+
+
+
+
---
+++ Print.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std; 
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout << "Helloworld\n";
+
+    cout << "Papop" << "Sangeamsak";
+    cout << "age" << "15";
+
+    //new row
+
+    cout<< endl;
+    cout << "Papop" << "\tSangeamsak\n";
+    cout << "age" << "\t15";
+
+    cout << endl;
+    cout << "Hello" << " Welcome to " << " Desneyland\n";
+
+}   
+
+
+
+//ย่อ std  namespace    = using namespace std; 
+
+//// comment 
+/*
+ multi comment 
+*/
