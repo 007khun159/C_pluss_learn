93793c828a57ebfc0683444541ce31c11aa3c917
007khun159 <87358687+007khun159@users.noreply.github.com>
2022-12-06 20:31:11 +0700

    Add files via upload

---
+++ condition/Nest_if_else.cpp
@@ -0,0 +1,72 @@
+﻿// Nest_if_else
+#include <iostream>
+using namespace std; 
+
+
+
+//ถ้าชำระเงินเกืน 15,000 บาท จะได้ระบส่วนลดแต่ถ้าน้อยกว่าจะได้รับส่วนลด
+
+
+int main()
+{
+	
+	int amout = 0 ,price_sale;
+
+
+
+	/// INPUT
+	cout << "Input amount : ";
+	cin >> amout;
+
+
+	// OUTPUT
+	cout << "---------Description---------------" << endl;
+	
+	cout << "The amout of price is " << amout << endl;
+
+ 
+	if (amout >= 1500) {
+
+
+		if (amout == 15000){
+		
+			price_sale = 0.1 * 15000;
+		
+			cout << "Your price is " << amout - price_sale << endl;
+		
+		
+		}
+		else if (amout == 20000)
+		{
+
+			price_sale = 0.2 * 20000;
+
+			cout << "Your price is " << amout - price_sale << endl;
+
+
+		}
+		else if (amout == 30000)
+		{
+
+			price_sale = 0.3 * 30000;
+
+			cout << "Your price is " << amout - price_sale << endl;
+
+
+		}
+
+
+
+
+
+	}
+	else {
+
+		cout << "Your price is : " << amout << endl;
+
+
+	}
+
+
+}
+
---
+++ condition/swithch_case.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+using namespace std;
+
+
+// จะมีการเลือกเงื่อนไขแบบ 2 style 
+// 1. if
+// 2. switch
+// 3. case แบบหลายเงื่อนไข
+int main()
+{	/*
+		int sevice;
+
+		// if - else  stlye
+		cout << "Input your server number (1-2) : ";
+		cin >> sevice;
+
+		if (sevice == 1) {
+
+			cout << "Creat New Bank Accout";
+		}
+		else if (sevice == 2) {
+
+			cout << "Withdraw and Deposite";
+
+		}
+		else {
+			cout << "Invalid choice";
+		}
+
+		cout << endl;
+
+		// switch style
+		// กรณีของ c++ ตัวนี้จะดีกว่า
+
+		switch (sevice) {
+			case 1 :
+
+				cout << "Creat New Bank Accout";
+				break;
+
+			case 2 :
+				cout << "Withdraw and Deposite";
+				break;
+
+
+			default:
+				cout << "Invalid choice ";
+				break;
+
+
+		}
+	*/
+
+	// Example Exit Program
+
+   /*
+	   char confirm;
+	   cout << "Do you want to exit program (y/n) : ";
+	   cout << "Enter for confirm : ";
+	   cin >> confirm;
+
+	   switch (confirm) {
+			   case 'y' :
+				   cout << "Exit.......";
+				   break;
+			   case  'n':
+				   cout << "Still on program";
+				   break;
+
+			   default:
+				   cout << " Invalid Function";
+				   break;
+   }
+
+
+   */
+
+
+   // case แบบหลายเงื่อนไข
+   //กรณีคล้ายๆเดิมแต่จะเพิ่ม case หลายกรณีมากยิ่งขึ้น
+
+
+		char confirm;
+		cout << "Do you want to exit program (y/n) : ";
+		cout << "Enter for confirm : ";
+		cin >> confirm;
+
+		switch (confirm) {
+		case 'y':  case'Y':
+			cout << "Exit.......";
+			break;
+		case  'n': case 'N':
+			cout << "Still on program";
+			break;
+
+		default:
+			cout << " Invalid Function";
+			break;
+
+		}
+
+}
